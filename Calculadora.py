2a35675b20776440f06a56fde1123825c3498d01
Will1-py <guerracuadroswilfrandavid@gmail.com>
2026-06-11 08:41:59 -0500

    Calculadora.py
    Se le añadio la opcion de Potencia.

--- Calculadora.py
+++ Calculadora.py
@@ -8,8 +8,9 @@ print("1. Suma")
 print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
+print("5. Potencia")
 
-opcion = input("Ingrese el número de la operación (1-4): ")
+opcion = input("Ingrese el número de la operación (1-5): ")
 
 if opcion == "1":
     resultado = num1 + num2
@@ -29,6 +30,10 @@ elif opcion == "4":
         print("Resultado:", resultado)
     else:
         print("Error: No se puede dividir entre cero.")
+
+elif opcion == "5":
+    resultado = num1 ** num2
+    print("Resultado:", resultado)
 
 else:
     print("Opción no válida.")
